f3f2196696f238ce33e6feac56b9fc22fb8d663a
Chukwuma Akaogu <chukw@DESKTOP-FFVVNOB>
2026-09-09 11:27:26 -0700

    sa

--- TaskApp/src/Components/MyApp.jsx
+++ TaskApp/src/Components/MyApp.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./MyApp.css";
-import sali from "./salford.png";
+import sali from "./Sali.png";
 export default function MyApp() {
     return (
         <section>
