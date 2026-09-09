af8ebc6be77f4c6c69c283392b718bd931905f63
Chukwuma Akaogu <chukw@DESKTOP-FFVVNOB>
2026-09-09 14:09:56 -0700

    christmas

--- TaskApp/src/Components/MyApp.jsx
+++ TaskApp/src/Components/MyApp.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./MyApp.css";
-import sali from "./Sali.png";
+import sali from "./Chrichri2.pdf";
 export default function MyApp() {
     return (
         <section>
             <div>
-                <img src={sali} alt="Salford"/>
+                <img src={sali} alt="Chrichri2"/>
             </div>
         </section>
     )
