7fc210d5a8f1eef2c8ef4a013c175a4c91577ac0
Chukwuma Akaogu <chukw@DESKTOP-FFVVNOB>
2026-09-09 10:58:44 -0700

    hhhh

--- TaskApp/src/Components/MyApp.jsx
+++ TaskApp/src/Components/MyApp.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./MyApp.css";
-import logo from "./salford.CorelDRAW 2026 Graphic";
+import logo from "./salford.png";
 export default function MyApp() {
     return (
         <section>
