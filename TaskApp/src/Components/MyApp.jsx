5601cc438d6669c7fbace2c90784619b46e422f5
Chukwuma Akaogu <chukw@DESKTOP-FFVVNOB>
2026-09-19 11:27:12 -0700

    tilt

--- TaskApp/src/Components/MyApp.jsx
+++ TaskApp/src/Components/MyApp.jsx
@@ -5,7 +5,7 @@ export default function MyApp() {
     return (
         <section>
             <div>
-                <img src={MikesClothes} alt="Mikes Clothes" width="650" height="900" />
+                <img src={MikesClothes} alt="MikesClothes" width="650" height="900" />
             </div>
         </section>
     )
