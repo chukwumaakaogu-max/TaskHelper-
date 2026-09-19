ed660d506a9c917f0bdb85712442c6e8243de4de
Chukwuma Akaogu <chukw@DESKTOP-FFVVNOB>
2026-09-19 11:14:04 -0700

    hhhjjjkkklll

--- TaskApp/src/Components/MyApp.jsx
+++ TaskApp/src/Components/MyApp.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./MyApp.css";
-import sali from "./chichi.png";
+import MikesClothes from "./Mikes Clothes.png";
 export default function MyApp() {
     return (
         <section>
             <div>
-                <img src={sali} alt="chichi" width="650" height="900" />
+                <img src={MikesClothes} alt="Mikes Clothes" width="650" height="900" />
             </div>
         </section>
     )
